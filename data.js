d88d5cd19dbdbefa6276e94baef85b2e29718c33
Anant Kaushik <anantkaushik@anants-mbp.wifi.cmu.edu>
2020-01-13 12:51:05 -0500

    start sending data

---
+++ data.js
@@ -0,0 +1,9 @@
+const data = {
+    name: "Anant Kaushik",
+    job: "Masters Student",
+    dob: "01 August 1996",
+    email: "anantk@cs.cmu.edu",
+    skype: "live:anantkaushik.me",
+}
+
+module.exports = data;
